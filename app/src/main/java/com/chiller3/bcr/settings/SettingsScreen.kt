e8b33b02052d194bde6c7906efda939a49a5cd70
aksb <42714906+aksb@users.noreply.github.com>
2026-09-18 02:34:20 +0800

    v4.42

--- app/src/main/java/com/chiller3/bcr/settings/SettingsScreen.kt
+++ app/src/main/java/com/chiller3/bcr/settings/SettingsScreen.kt
@@ -1035,6 +1035,7 @@ private fun PreviewSettingsScreen() {
                 forceDirectBoot = false,
                 onCallRecordingChange = {},
                 onRecordRulesSettings = {},
+                onPermissionsSettings = {},
                 onOutputDirSettings = {},
                 onOutputDirOpen = {},
                 onOutputFormatSettings = {},
